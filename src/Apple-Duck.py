ea29f7b41a9e05f7be156c54a68324ecb2c36b03
Sienna Martinez <smm210018@utdallas.edu>
2023-12-03 12:09:56 -0600

    added win and lose screen and conditions

--- src/Apple-Duck.py
+++ src/Apple-Duck.py
@@ -23,6 +23,10 @@ carrot_id = 0
 time = 0
 carrot_time = 0
 apple_time = 0
+lose_screen = False
+win_screen = False
+apple_count = 0
+carrot_count = 0
 
 offsets = {
     "up": (0, 20),
@@ -42,6 +46,10 @@ apple_item = pygame.image.load("assets/apple_cropped.png")
 apple_item = pygame.transform.scale (apple_item, (50,50))
 carrot_item = pygame.image.load("assets/carrot_cropped.png")
 carrot_item = pygame.transform.scale (carrot_item, (50,50))
+win_game = pygame.image.load("assets/win_screen.png")
+win_game = pygame.transform.scale(win_game, (WIDTH, HEIGHT))
+lose_game = pygame.image.load("assets/lose_screen.png")
+lose_game = pygame.transform.scale (lose_game, (WIDTH,HEIGHT))
 player_y = HEIGHT - player_duck.get_height()
 for i in range (1,1):
     bgs.append(pygame.image.load(f'assets/bgs/{i}.png'))
@@ -49,30 +57,36 @@ def spawn_apple():
     global apple_id
     w = apple_item.get_width()
     x = random.randint(0,WIDTH - w)
-    y = HEIGHT - player_duck.get_height() - 80
+    y = HEIGHT - player_duck.get_height() - 90
     apples.append((time,x,y))
 def spawn_carrot():
     global carrot_id
     w = carrot_item.get_width()
     x = random.randint(0,WIDTH - w)
-    y = HEIGHT - player_duck.get_height() - 60
+    y = HEIGHT - player_duck.get_height() - 80
    
     carrots.append((time,x,y))
 spawn_carrot()
 spawn_apple()
 def apple_collision():
+    global carrot_count, apple_count
     player_rect = pygame.Rect(player_x, player_y, player_duck.get_width(), player_duck.get_height())
     for i in reversed (range(len(apples))):
         apple = apples [i]
         apple_rect = pygame.Rect(apple[1], apple[2], apple_item.get_width(), apple_item.get_height())
-        if player_rect.colliderect(apple_rect) or time> apple[0]+3000 :
+        if player_rect.colliderect(apple_rect):
+            del apples [i]
+            apple_count += 1
+        if time> apple[0]+3000 :
             del apples [i]
     for i in reversed (range (len(carrots))):
         carrot = carrots [i]
         carrot_rect = pygame.Rect(carrot[1],carrot[2], carrot_item.get_width(), carrot_item.get_height())
-        if player_rect.colliderect(carrot_rect) or time> carrot[0]+3000:
+        if player_rect.colliderect(carrot_rect):
             del carrots [i]
-
+            carrot_count += 1
+        if time> carrot[0]+3000:
+            del carrots [i]
 def draw_image(img, x, y ):
     rect = img.get_rect()
     rect.move((x, y))
@@ -84,9 +98,20 @@ def draw():
         draw_image(apple_item, apple[1],apple[2])
     for carrot in carrots:
         draw_image(carrot_item,carrot[1],carrot[2])
+    if apple_count == 5:
+        draw_image(win_game, 0,0)
+    if carrot_count == 1:
+        draw_image(lose_game, 0,0)
+    player_rect = pygame.Rect(player_x, player_y, player_duck.get_width(), player_duck.get_height())
     pygame.display.flip()
 def update():
-    global player_x , movement_speed, grounded, player_y, acc, jumping, jump_speed, time
+    global player_x , movement_speed, grounded, player_y, acc, jumping, jump_speed, time, win_screen, lose_screen
+    if apple_count == 5:
+        win_screen = True
+    if carrot_count == 1:
+        lose_screen = True
+    if win_screen or lose_screen:
+        return
     player_x += side_to_side * movement_speed
     floor = HEIGHT-player_duck.get_height()
     grounded = abs(floor-player_y)<5
@@ -114,6 +139,7 @@ def update():
         carrot_time = 0
         spawn_carrot()
     timer.tick(fps)
+
 running= True
 while running:
     for event in pygame.event.get():
@@ -133,5 +159,6 @@ while running:
                 side_to_side = 0
             if event.key == pygame.K_UP:
                 jumping = False
+                
     update()
     draw()
